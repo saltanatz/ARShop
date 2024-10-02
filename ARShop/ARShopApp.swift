7c7bd20d17ed7484c09becee62a40246709a8096
saltanatz <saltanat.zarkhinova@nu.edu.kz>
2024-10-02 15:13:59 +0500

    Initial Commit

---
+++ ARShop/ARShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARShopApp.swift
+//  ARShop
+//
+//  Created by Saltanat on 02.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ARShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
